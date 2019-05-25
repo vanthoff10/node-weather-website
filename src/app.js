6061114f2dccf45560e4d95ed9f15019a1f6dddb
vanthoff10 <ashwinpanwar1019999@gmail.com>
2019-05-26 00:32:28 +0530

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,85 @@
+const path = require('path')
+const express  =require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+       title: 'Weather', 
+       name: 'VantHoff (Ashwin Panwar)'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title:'About', 
+        name:'Ashwin Panwar'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: 'You can get help here',
+        name: 'Ashwin Panwar VantHoff'
+    })
+})
+app.get('/weather', (req, res) =>{
+    if(!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })       
+    }
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error){
+            return res.send({ error})
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({ error })
+            }
+
+            res.send ({
+                forecast: forecastData,
+                location,
+                address: req.query.address   
+            })
+        })
+    })
+   
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'Youmust provide a search term'
+        })
+    }
+
+
+    res.send({
+        products: []
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is Running on port 3000')
+})
